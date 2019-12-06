af22ea71f8420e25fd5aad635072587d1426f700
JCathey10 <35239619+JCathey10@users.noreply.github.com>
2019-12-05 19:34:07 -0800

    Add files via upload

---
+++ FootyFans/FootyFans/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootyFans.Models
+{
+	public class Comment
+	{
+		public int CommentId { get; set; }
+		public User UserName { get; set; }
+		public string CommentText { get; set; }
+	}
+}
---
+++ FootyFans/FootyFans/Models/Video.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootyFans.Models
+{
+	public class Video
+	{
+		private List<Comment> comments = new List<Comment>();
+		public List<Comment> Comments { get { return comments; } }
+
+		public int VideoID { get; set; }
+		public string VideoUrl { get; set; }
+		public string Description { get; set; }
+	}
+}
